40f18004e01f94abd6693a73be7ca395d3311ac3
andresmgomez <andresmgomez23@gmail.com>
2023-05-01 12:42:32 -0500

    Add google fonts and styling to profile card

--- exercises/profile-card.svelte
+++ exercises/profile-card.svelte
@@ -3,48 +3,105 @@
   let profilePic = "https://randomuser.me/api/portraits/women/25.jpg";
 </script>
 
-<div class="profile-card">
-  <div class="container-card">
-    <h1 class="profile-name">
-      Welcome, <strong>{customer}</strong>
-    </h1>
-    <div class="wrapper-image">
-      <img src={profilePic} alt="A randomly generate female profile" />
+<section class="profile-area">
+  <div class="profile-card">
+    <div class="content">
+      <div class="profile-image">
+        <img src={profilePic} alt={customer} />
+      </div>
+      <div class="profile-info">
+        <h2>{customer}</h2>
+        <br />
+        <span>Digital Marketing</span>
+      </div>
+      <span class="toggle-btn" />
     </div>
   </div>
-</div>
+</section>
 
 <style>
-  .profile-card {
-    width: 100%;
-    height: 178px;
-    background-color: orange;
+  @import url("https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900");
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Poppins", Arial, sans-serif;
   }
 
-  .container-card {
+  .profile-area {
     display: flex;
     justify-content: center;
     align-items: center;
-    padding-top: 30px;
+    min-height: 100vh;
+    background-color: #f1f1f1;
   }
-
-  .profile-name {
-    padding: 15px;
-    font-size: 36px;
-    color: white;
+  .profile-card {
+    height: 100px;
+    transition: 0.5s;
   }
-
-  .profile-name strong {
-    font-weight: 300;
-  }
-
-  .wrapper-image {
+  .profile-card .content {
+    position: relative;
+    width: 400px;
+    min-height: 150px;
+    padding: 60px 40px 30px;
     display: flex;
-    justify-self: center;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    background-color: orange;
+    border-radius: 10px;
   }
-
-  .wrapper-image img {
+  .profile-card .profile-image {
+    position: absolute;
+    top: 0;
+    width: 100px;
+    height: 100px;
     border-radius: 50%;
-    border: 1px solid #fff;
+    border: 6px solid #fff;
+    transform: translateY(-50%);
+    transition: all 0.5s ease;
+    overflow: hidden;
+    z-index: 9;
+  }
+  .profile-card .profile-image img {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  .profile-card .profile-info {
+    position: relative;
+    text-align: center;
+  }
+  .profile-card .profile-info h2 {
+    font-size: 1.25em;
+    font-weight: 500;
+    line-height: 1.05em;
+    color: #fff;
+  }
+  .profile-card .profile-info span {
+    font-size: 0.89em;
+    font-weight: 300;
+    color: #f1f1f1;
+  }
+  .profile-card .toggle-btn {
+    position: absolute;
+    bottom: 0;
+    width: 120px;
+    padding: 5px 15px;
+    font-size: 0.8em;
+    text-align: center;
+    background-color: #fff;
+    color: orange;
+    border: 6px solid #f2eded;
+    border-radius: 30px;
+    transform: translateY(50%);
+    transition: all 0.5s ease-out;
+    cursor: pointer;
+  }
+  .profile-card .toggle-btn::before {
+    content: "More Info";
   }
 </style>
